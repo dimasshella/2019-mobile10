ce2b843d999f867f6fda4d6e2b85db31268e674d
shella <dimasshella04@gmail.com>
2019-10-31 12:56:53 +0700

    ColorChangeNormal

--- SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
+++ SkorBasket/app/src/main/java/id/ac/polinema/skorbasket/viewmodels/SharedScore.java
@@ -1,4 +1,8 @@
 package id.ac.polinema.skorbasket.viewmodels;
 
-public class SharedScore {
+public class SharedScore extends AndroidviewModel {
+    private MutableLiveData<Integer> scoreHome;
+    private MutableLiveData<Integer> scoreVisitor;
+    private MutableLiveData<Boolean> winner;
 }
+
